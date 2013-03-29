75bd7ae434409aec19b29336ee786dab82c07ba0
Anusorn <gefoonp@gmail.com>
2013-03-29 09:34:04 +0700

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,14 @@
+  		<div class="footer">
+				<!-- Copyright -->
+				<p class="copyright">
+					&copy; <?php echo date("Y"); ?> Copyright <?php bloginfo('name'); ?>.
+				</p>
+				<!-- /Copyright -->
+			</div>
+
+<script>window.jQuery || document.write('<script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/1.9.1/jquery.min.js"><\/script>');</script>
+		
+
+<?php wp_footer(); ?>
+</body>
+</html>
