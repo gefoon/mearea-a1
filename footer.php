9c077fd60dc4130d0d5c57dea71780255229970e
Anusorn <gefoonp@gmail.com>
2013-03-29 09:36:15 +0700

    Update footer.php

--- footer.php
+++ footer.php
@@ -6,7 +6,7 @@
 				<!-- /Copyright -->
 			</div>
 
-<script>window.jQuery || document.write('<script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/1.9.1/jquery.min.js"><\/script>');</script>
+
 		
 
 <?php wp_footer(); ?>
